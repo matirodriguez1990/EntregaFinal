f12ae1528e6f6defb10e45576c6e49cb34cd2dc1
Matias Rodriguez <matirodriguez1990@gmail.com>
2022-05-21 15:27:08 -0300

    correcciones menores

--- AppJuegos/forms.py
+++ AppJuegos/forms.py
@@ -5,18 +5,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-<<<<<<< HEAD
-from AppJuegos.models import Post, Juego
-"""from django.contrib.auth import authenticate, get_user_model
-from django.contrib.auth.models import User
-from django.utils.text import capfirst
-from django.utils.translation import gettext_lazy as _
-"""
-
-=======
-
-from AppJuegos.models import Consola, Post, Juego
->>>>>>> 6c3e1b3f2b3034e8c922d89ee1437cbf09963954
+from AppJuegos.models import Post, Juego, Consola
 
 def validate_email(request):
     if User.objects.filter(email = request).exists():
@@ -102,59 +91,6 @@ class JuegoEditFormulario(forms.ModelForm):
             'genero': forms.TextInput(attrs={'class':'form-control'}),
         }
 
-<<<<<<< HEAD
-"""
-class LoginFormulario(forms.Form):
-    username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'class':'form-control'}))
-
-    error_messages = {
-        'invalid_login': _("Credenciales incorrectas"),
-        'inactive': _("Cuenta inactiva"),
-    }
-
-    def __init__(self, request=None, *args, **kwargs):
-        self.request = request
-        self.user_cache = None
-        super(AuthenticationForm, self).__init__(*args, **kwargs)
-        UserModel = get_user_model()
-        self.username_field = UserModel._meta.get_field(UserModel.USERNAME_FIELD)
-        if self.fields['username'].label is None:
-            self.fields['username'].label = capfirst(self.username_field.verbose_name)
-
-    def clean(self):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
-
-        if username and password:
-            self.user_cache = authenticate(username=username,
-                                           password=password)
-            if self.user_cache is None:
-                raise forms.ValidationError(
-                    self.error_messages['invalid_login'],
-                    code='invalid_login',
-                    params={'username': self.username_field.verbose_name},
-                )
-            else:
-                self.confirm_login_allowed(self.user_cache)
-
-        return self.cleaned_data
-
-    def confirm_login_allowed(self, user):
-        if not user.is_active:
-            raise forms.ValidationError(
-                self.error_messages['inactive'],
-                code='inactive',
-            )
-
-    def get_user_id(self):
-        if self.user_cache:
-            return self.user_cache.id
-        return None
-
-    def get_user(self):
-        return self.user_cache"""
-=======
 class ConsolaFormulario(forms.ModelForm):
     class Meta:
         model = Consola
@@ -166,4 +102,3 @@ class ConsolaFormulario(forms.ModelForm):
             'precio': forms.NumberInput(attrs={'class':'form-control'}),
             'unidadesVendidas' : forms.NumberInput(attrs={'class':'form-control'}),
         }
->>>>>>> 6c3e1b3f2b3034e8c922d89ee1437cbf09963954
